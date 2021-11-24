8fdede353b4b7e0da997d5d0d649d7928981d272
Simone Menna <simonemennafiverr@gmail.com>
2021-11-24 14:46:18 +0100

    Started Working on Vector interface

--- includes/Vec_interface.hpp
+++ includes/Vec_interface.hpp
@@ -8,7 +8,17 @@
 namespace ft {
     template<typename T, typename Allocator = std::allocator<T>>
     class vector {
-        
+    public:
+        typedef T                                           value_type;
+        typedef Allocator                                   allocator_type;   
+        typedef typename allocator_type::reference			reference;
+	    typedef typename allocator_type::const_reference	const_reference;
+	    typedef typename allocator_type::pointer			pointer;
+	    typedef typename allocator_type::const_pointer		const_pointer;
+
+	    typedef ptrdiff_t									difference_type;
+	    typedef size_t										size_type;
+
     };
 }
 #endif
